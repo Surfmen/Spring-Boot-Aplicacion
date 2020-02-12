fd6c48b3262157940a6ebaf384cdbfe9a5a7a3b3
Surfmen <surfmen@hotmail.com>
2020-02-12 11:33:47 +0100

    Completado, problema Error 404. Closed #3

--- SpringBootApp/src/main/java/es/peonadas/springbootapp/SpringBootAppApplication.java
+++ SpringBootApp/src/main/java/es/peonadas/springbootapp/SpringBootAppApplication.java
@@ -1,9 +1,14 @@
 package es.peonadas.springbootapp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan(basePackages = "es.peonadas.springbootapp.controller")
 public class SpringBootAppApplication {
 
 	public static void main(String[] args) {
